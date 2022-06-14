55734ca4bbda1a688736f89bc94a0be74fb2129c
Ольга <devyatovka.0993@mail.ru>
2022-06-14 21:41:57 +0300

    add_img

--- vebsite/main/views.py
+++ vebsite/main/views.py
@@ -4,7 +4,6 @@ from .models import Task
 
 def index(request):
     task=Task.objects.all()
-
     return render(request, 'main/index.html',{'title':'Главная страница','tasks':'title'})
 
 def about(request):
