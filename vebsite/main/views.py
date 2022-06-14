58144f6eb14362b17c479d8a486c79fa11628136
Ольга <devyatovka.0993@mail.ru>
2022-06-15 01:43:29 +0300

    date_site

--- vebsite/main/views.py
+++ vebsite/main/views.py
@@ -4,7 +4,7 @@ from .models import Task
 
 def index(request):
     task=Task.objects.all()
-    return render(request, 'main/index.html',{'title':'Главная страница','tasks':'title'})
+    return render(request, 'main/index.html',{'title':'Главная страница','tasks':task})
 
 def about(request):
     return render(request, 'main/about.html')
