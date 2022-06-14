48a3bdba85a24b702e706fb79c4545a4b71addd8
Ольга <devyatovka.0993@mail.ru>
2022-06-14 12:39:52 +0300

    add_fields

--- vebsite/main/models.py
+++ vebsite/main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Task(models.Model):
     title=models.CharField('Название', max_length=50)
     task=models.TextField('Описание')
+    technology = models.CharField('Технологии', max_length=50)
+    image = models.FileField(upload_to='img/')
 
     def __str__(self):
         return self.title
