df4567c479a077e58c0ab10c106df3b72c7e859b
Ольга <devyatovka.0993@mail.ru>
2022-06-14 12:08:39 +0300

    add_fields

--- vebsite/main/urls.py
+++ vebsite/main/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path ('',views.index ),
-    path ('about', views.about),
-    path ('home/', views.home),
-    path ('new-page', views.new),
+    path ('',views.index, name='home' ),
+    path ('about', views.about, name='about'),
+
 ]
--- vebsite/main/views.py
+++ vebsite/main/views.py
@@ -7,9 +7,4 @@ def index(request):
 def about(request):
     return render(request, 'main/about.html')
 
-def home(request):
-    return HttpResponse('<h2>HomeWork</h2>')
-
-def new(request):
-    return render(request, 'new_page.html')
 
