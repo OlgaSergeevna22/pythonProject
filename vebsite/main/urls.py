3a44690474aa26eb54ce24aaa47552a0f717b6c2
Ольга <devyatovka.0993@mail.ru>
2022-06-15 21:42:19 +0300

    add_page

--- vebsite/main/urls.py
+++ vebsite/main/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path ('',views.index, name='home' ),
     path ('about', views.about, name='about'),
+    path ('create', views.create, name='create'),
 
 ]
 
