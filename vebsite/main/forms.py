49f5e6c831c1d5e0a7b3c715e648b57fd988ab14
Ольга <devyatovka.0993@mail.ru>
2022-06-16 11:57:34 +0300

    add_forms

--- vebsite/main/forms.py
+++ vebsite/main/forms.py
@@ -1,7 +1,11 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title','task','technology']
+        widgets = {'title':TextInput(attrs={'class':'form-control','placeholder':'Введите название'}),
+                   'task': TextInput(attrs={'class': 'form-control','placeholder': 'Введите описание'}),
+                   'technology': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите технологию'})
+                   }
