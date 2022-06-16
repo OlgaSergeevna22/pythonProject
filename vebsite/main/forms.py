e547d11cd017e0db5fdba5c8371ff032804af2fd
Ольга <devyatovka.0993@mail.ru>
2022-06-16 13:46:37 +0300

    tamplase_fields

--- vebsite/main/forms.py
+++ vebsite/main/forms.py
@@ -1,11 +1,11 @@
 from .models import Task
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, Textarea
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title','task','technology']
         widgets = {'title':TextInput(attrs={'class':'form-control','placeholder':'Введите название'}),
-                   'task': TextInput(attrs={'class': 'form-control','placeholder': 'Введите описание'}),
+                   'task': Textarea(attrs={'class': 'form-control','placeholder': 'Введите описание'}),
                    'technology': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите технологию'})
                    }
